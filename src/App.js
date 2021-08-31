b585837ce95876cfeb04f4807a7615cc803ed0fa
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-08-31 21:31:35 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Feedback from './components/Feedback/FeedbackOptions'
 
 import Statistics from './components/Statistics/Statistics';
+
 import SectionTitle from './components/SectionTitle/SectionTitle';
+import Notification from './components/Notification/Notification';
 import './App.css';
 
 class App extends React.Component{
@@ -32,6 +34,7 @@ return Math.round(good*100/(good+neutral+bad))
   }
   render() {
     const { good, neutral, bad } = this.state
+    const totalFeedback = good+neutral+bad
       
         return (
           <div>
@@ -39,7 +42,11 @@ return Math.round(good*100/(good+neutral+bad))
             <Feedback options={Object.keys(this.state)} handleIncrement={this.handleIncrement}/>
             </SectionTitle>
             <SectionTitle title='Statistics'>
-            <Statistics good={good} neutral={neutral} bad={bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()}/>
+              {totalFeedback > 0 ?
+                <Statistics good={good} neutral={neutral} bad={bad} totalFeedback={totalFeedback}
+                  total={this.countTotalFeedback()}
+                  positivePercentage={this.countPositiveFeedbackPercentage()}/> : <Notification message="No feedback given" />}
+            
           </SectionTitle>
           </div>
     
